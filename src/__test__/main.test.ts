1d94727c0af3b83f64afbce39653909b9d4a4500
Scott Ross <scott.william.ross2@gmail.com>
2022-10-13 23:33:53 -0400

    wrote renderPreferment and a passing test

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,19 +1,37 @@
-import {beforeEach, describe, expect, it} from 'vitest';
+import {  describe, expect, it } from 'vitest';
+import { renderPreferment } from "../main";
 //const jsdom = require("jsdom");
 //const { JSDOM } = jsdom;
 
 // @vitest-environment jsdom
 describe('test script', () => {
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', 'preferment-checkbox');
+    checkbox.addEventListener('click', renderPreferment);
+    document.body.appendChild(checkbox);
 
-    beforeEach(() => {
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        checkbox.setAttribute('id', 'preferment-checkbox')
-        document.body.appendChild(checkbox);
-    })
+    const prefermentCheckbox = document.getElementById("preferment-checkbox");
 
     it('test if preferment-checkbox exists', () => {
-        const element = document.getElementById("preferment-checkbox");
-        expect(element).not.toBeNull();
+        expect(prefermentCheckbox).not.toBeNull();
+    });
+
+    it('test if function renders label and input', () => {
+        const form = document.createElement('form');
+        const waterLabel = document.createElement('label');
+        waterLabel.setAttribute('id', 'water-label');
+        form.appendChild(waterLabel);
+        document.body.appendChild(form);
+        renderPreferment();
+        const input = document.getElementById("preferment");
+        expect(input).not.toBeNull();
+    });
+
+    it.todo('test if preferment input is rendered if checkbox is checked', () => {
+        prefermentCheckbox!.click();
+        const preferment = document.getElementById("preferment");
+        expect(preferment).not.toBeNull();
     })
-})
+
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import './style.css'
+export const renderPreferment = () => {
+    const label = document.createElement('label');
+    label.innerHTML = `
+        Preferment Temperature
+        <input id="preferment" type="number">
+    `;
+    const form = document.getElementsByTagName('form').item(0);
+    const waterLabel = document.getElementById('water-label');
+    const br = document.createElement('br');
+    label.insertAdjacentElement('afterend', br);
+    form!.insertBefore(label, waterLabel);
+}
